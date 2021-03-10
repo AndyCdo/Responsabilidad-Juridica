f7695ad573b25e39a1e6494cfbd2cccda55da5ab
Andrea <chela355@hotmail.com>
2021-03-10 15:35:51 -0500

    se hizo el ejercicio

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Responsabilidad_Juridica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Que tipo de persona es: ");
+            string personaNoJ= Convert.ToString(Console.ReadLine());
+            string mensaje= "";
+            mensaje= tipoPersona(personaNoJ);
+            Console.WriteLine(mensaje);
+        }
+        static string tipoPersona(string persona){
+            if (persona == "Persona Natural"){
+                return "No Declara Renta";
+            }
+            else if (persona == "Persona Juridica"){
+                return "Declara Renta";
+            }
+            else
+            {
+                return " ";
+            }
+        }
+    }
+}
